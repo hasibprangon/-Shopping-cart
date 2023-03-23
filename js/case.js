429cc7764ca563a28cd16ca4fc9fccc1aabb64fb
hasibprangon <hasibulhossainp09@gmail.com>
2023-03-23 19:36:29 +0600

    increment and deccrement of mobile case and little error handling

--- js/case.js
+++ js/case.js
@@ -1,21 +1,8 @@
-document.getElementById('btn-case-plus').addEventListener('click', function () {
-    // console.log('btn clicked');
-  
+document.getElementById('btn-case-plus').addEventListener('click', function () {;
+  updateCaseNumber(true);
 })
 
 
 document.getElementById('btn-case-minus').addEventListener('click', function () {
-    // console.log('btn clicked')
-
-    const caseNumberFieldMinus = document.getElementById('case-number-field');
-    const caseNumberFieldMinusStr = caseNumberFieldMinus.value;
-    const prevCasenumberFieldMinus = parseInt(caseNumberFieldMinusStr);
-    // console.log(prevCasenumberFieldMinus);
-    const newCaseNumberFieldMinus = prevCasenumberFieldMinus - 1;
-    caseNumberFieldMinus.value = newCaseNumberFieldMinus;
-    if (newCaseNumberFieldMinus < 0) {
-        // prevCasenumberFieldMinus = 0;
-        caseNumberFieldMinus.value = 0;
-        alert('ERROR, you can not buy a product less than zero')
-    }
+    updateCaseNumber(false); 
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,8 +1,15 @@
-function updateCaseNumber (){
+function updateCaseNumber (isIncrease){
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberFieldStr = caseNumberField.value;
     const prevCasenumberField = parseInt(caseNumberFieldStr);
-    // console.log(prevCasenumberField);
-    const newCaseNumberField = prevCasenumberField + 1;
+    let newCaseNumberField ;
+    if(isIncrease === true || prevCasenumberField < 1 ){
+        alert('ERROR, You cannot buy less than zero quantity of products')
+        newCaseNumberField = prevCasenumberField + 1;
+       
+    }
+    else{
+        newCaseNumberField = prevCasenumberField - 1;
+    }
     caseNumberField.value = newCaseNumberField
 }
