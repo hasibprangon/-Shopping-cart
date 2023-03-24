bbf1aa80b1c38416495f95ec82115ef53e88066c
hasibprangon <hasibulhossainp09@gmail.com>
2023-03-24 16:53:58 +0600

    increasing case price and error handling

--- js/case.js
+++ js/case.js
@@ -1,8 +1,15 @@
-document.getElementById('btn-case-plus').addEventListener('click', function () {;
-  updateCaseNumber(true);
+
+
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+  ;
+  const newCaseNumberField = updateCaseNumber(true);
+
+  const caseTotalPrice = newCaseNumberField * 59;
+  const caseTotalElement = document.getElementById('case-total');
+  caseTotalElement.innerText = caseTotalPrice;
 })
 
 
 document.getElementById('btn-case-minus').addEventListener('click', function () {
-    updateCaseNumber(false); 
+  updateCaseNumber(false);
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,15 +1,23 @@
-function updateCaseNumber (isIncrease){
+function updateCaseNumber(isIncrease) {
     const caseNumberField = document.getElementById('case-number-field');
     const caseNumberFieldStr = caseNumberField.value;
     const prevCasenumberField = parseInt(caseNumberFieldStr);
-    let newCaseNumberField ;
-    if(isIncrease === true || prevCasenumberField < 1 ){
-        alert('ERROR, You cannot buy less than zero quantity of products')
+    let newCaseNumberField;
+    if (isIncrease === true) {
+        // prevCasenumberField < 1 
+
         newCaseNumberField = prevCasenumberField + 1;
-       
+
     }
-    else{
+    else if (prevCasenumberField < 1) {
+        alert('ERROR!, You cannot buy less than zero quantity of products');
+        newCaseNumberField = 0;
+
+    }
+    else {
         newCaseNumberField = prevCasenumberField - 1;
     }
     caseNumberField.value = newCaseNumberField
+
+    return newCaseNumberField;
 }
